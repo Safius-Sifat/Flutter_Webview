4b85064cc2f2e194cb091a1d3bd52938909803aa
Safius Sifat <safiussifat@gmail.com>
2023-06-07 03:04:30 +0600

    final app

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:webview_app/menu.dart';
+import 'package:webview_app/web_view_stack.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'navigation_controls.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+       debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home:const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+class _WebViewAppState extends State<WebViewApp> {
+
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://flutter.dev/'),
+      );
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Webview'),
+        actions: [
+          NavigationControls(controller: controller),
+          Menu(controller: controller),
+        ],
+      ),
+      body: WebViewStack(controller: controller,),
+    );
+  }
+}
